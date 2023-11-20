13f4f67f60f25117fbc1bc08d00379fdb8d106be
Komalpreet Singh <komalpreetsingh2305@gmail.com>
2023-11-20 01:35:51 -0500

    functionality complete

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ class Calculator {
     }
 
     delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
 
     }
 
@@ -22,6 +23,11 @@ class Calculator {
         if (number === "." && this.currentOperand.includes('.')) {
             return;
         }
+        if (this.currentOperand.split("")[0] === "0") {
+            this.currentOperand = ""
+
+        }
+        console.log(this.currentOperand.split(""));
         this.currentOperand = this.currentOperand.toString() + number.toString();
     }
 
@@ -64,10 +70,18 @@ class Calculator {
         this.previousOperand = '';
 
     }
+
+
     updateDisplay() {
 
         this.currentOperandText.innerText = this.currentOperand;
-        this.previousOperandText.innerText = this.previousOperand;
+        if (this.operation != null) {
+            this.previousOperandText.innerText = `${this.previousOperand} ${this.operation}`;
+        }
+        else {
+            this.previousOperandText.innerText = '';
+        }
+
         console.log("Update 2: " + this.currentOperand);
 
 
@@ -79,7 +93,7 @@ const numberButtons = document.querySelectorAll(`[data-number]`);
 const operationButtons = document.querySelectorAll(`[data-operation]`);
 const equalsButton = document.querySelector('[data-equal]');
 const deleteButton = document.querySelector(`[data-delete]`);
-const clearButton = document.querySelector(`[data-clear]`);
+const clearButton = document.querySelector(`[data-all-clear]`);
 const previousOperandText = document.querySelector(`[data-previousOperand]`);
 const currentOperandText = document.querySelector(`[data-currentOperand]`);
 
@@ -87,6 +101,7 @@ const calculator = new Calculator(previousOperandText, currentOperandText);
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
+
         calculator.appendNumber(button.innerText);
         console.log(button.textContent);
         calculator.updateDisplay();
@@ -103,5 +118,17 @@ operationButtons.forEach(button => {
 equalsButton.addEventListener('click', button => {
     console.log("equal click");
     calculator.compute();
+    calculator.updateDisplay();
+})
+
+clearButton.addEventListener('click', button => {
+    console.log("equal click");
+    calculator.clear();
+    calculator.updateDisplay();
+})
+
+deleteButton.addEventListener('click', button => {
+    console.log("equal click");
+    calculator.delete();
     calculator.updateDisplay();
 })
